e75055ea8c04eec66a88a2e40f008afd84519894
Man1ac-1773 <mayookh.ghosh@gmail.com>
2026-02-05 07:28:43 +0530

    Moved to cpp

--- boids.cpp
+++ boids.cpp
@@ -6,7 +6,7 @@
 #define HEIGHT 700
 #define WORLD_WIDTH 2000
 #define WORLD_HEIGHT 2000
-#define BOID_COUNT 200
+#define BOID_COUNT 600
 
 #define PERCEPTION_RADIUS 50
 #define MAX_SPEED 2.5f
@@ -18,46 +18,57 @@
 #define TRI_DIM 5.0f // length from center to vertice
 
 #define CAMERA_SPEED 1000.0f;
-typedef Vector2 Vec2;
+typedef Vector2 Vector2;
 typedef struct
 {
-    Vec2 pos;
-    Vec2 vel;
+    Vector2 pos;
+    Vector2 vel;
 } Boid;
 
 typedef struct triangle_vectors
 {
-    Vec2 v1;
-    Vec2 v2;
-    Vec2 v3; // anti-clockwise order
+    Vector2 v1;
+    Vector2 v2;
+    Vector2 v3; // anti-clockwise order
 } Triangle;
 // let dimension s = 2.0f;
 // we go 2.0f from pos in the direction of vel
 // then we rotate that by 120, then 120 again and go 2.0f in that side
 // use these three points to make triangle
-Vec2 limit(Vec2 v, float max)
+Vector2 limit(Vector2 v)
 {
     float m = Vector2Length(v);
-    if (m > max)
+    if (m > MAX_SPEED)
     {
-        v.x = (v.x / m) * max;
-        v.y = (v.y / m) * max;
+        v.x = (v.x / m) * MAX_SPEED;
+        v.y = (v.y / m) * MAX_SPEED;
     }
     return v;
 }
 
 Triangle UpdateTriangleBoid(Boid boid)
 {
-    Vec2 dir = Vector2Scale(Vector2Normalize(boid.vel), TRI_DIM);
+    Vector2 dir = Vector2Scale(Vector2Normalize(boid.vel), TRI_DIM);
     Triangle t;
-    t.v1 = Vector2Add(boid.pos, dir);
+    t.v1 = boid.pos + dir;
     dir = Vector2Rotate(dir, 120 * DEG2RAD);
-    t.v2 = Vector2Add(boid.pos, dir);
+    t.v2 = boid.pos + dir;
     dir = Vector2Rotate(dir, 120 * DEG2RAD);
-    t.v3 = Vector2Add(boid.pos, dir);
+    t.v3 = boid.pos + dir;
     return t;
 }
 
+void ClampToWorld(Boid* boid)
+{
+    if (boid->pos.x > WORLD_WIDTH)
+        boid->pos.x -= WORLD_WIDTH;
+    if (boid->pos.y > WORLD_HEIGHT)
+        boid->pos.y -= WORLD_HEIGHT;
+    if (boid->pos.x < 0)
+        boid->pos.x += WORLD_WIDTH;
+    if (boid->pos.y < 0)
+        boid->pos.y += WORLD_HEIGHT;
+}
 int main(void)
 {
     InitWindow(WIDTH, HEIGHT, "Boids");
@@ -67,12 +78,12 @@ int main(void)
 
     for (int i = 0; i < BOID_COUNT; i++)
     {
-        boids[i].pos = (Vector2) {rand() % WIDTH, rand() % HEIGHT};
+        boids[i].pos = (Vector2) {(float) (rand() % WIDTH), (float) (rand() % HEIGHT)};
         boids[i].vel = (Vector2) {((rand() % 100) / 50.0f - 1), ((rand() % 100) / 50.0f - 1)};
     }
     Camera2D camera = {0};
-    camera.target = (Vec2) {(float) WIDTH / 2, (float) HEIGHT / 2};
-    camera.offset = (Vec2) {(float) WIDTH / 2, (float) HEIGHT / 2};
+    camera.target = (Vector2) {(float) WIDTH / 2, (float) HEIGHT / 2};
+    camera.offset = (Vector2) {(float) WIDTH / 2, (float) HEIGHT / 2};
     camera.zoom = 1.0f;
     camera.rotation = 0.0f;
     while (!WindowShouldClose())
@@ -83,13 +94,13 @@ int main(void)
         camera.zoom += GetMouseWheelMove() * 0.1f;
         if (camera.zoom < 0.1f)
             camera.zoom = 0.1f;
-        if (IsKeyDown(KEY_RIGHT))
+        if (IsKeyDown(KEY_D))
             camera.target.x += GetFrameTime() * CAMERA_SPEED;
-        if (IsKeyDown(KEY_LEFT))
+        if (IsKeyDown(KEY_A))
             camera.target.x -= GetFrameTime() * CAMERA_SPEED;
-        if (IsKeyDown(KEY_UP))
+        if (IsKeyDown(KEY_W))
             camera.target.y -= GetFrameTime() * CAMERA_SPEED;
-        if (IsKeyDown(KEY_DOWN))
+        if (IsKeyDown(KEY_S))
             camera.target.y += GetFrameTime() * CAMERA_SPEED;
 
         for (int i = 0; i < BOID_COUNT; i++)
@@ -105,40 +116,31 @@ int main(void)
                 float d = Vector2Distance(boids[i].pos, boids[j].pos);
                 if (d < PERCEPTION_RADIUS && d > 0)
                 {
-                    Vector2 diff = Vector2Subtract(boids[i].pos, boids[j].pos);
-                    sep = Vector2Add(sep, Vector2Scale(diff, 1.0f / (d + 0.01f)));
-                    ali = Vector2Add(ali, boids[j].vel);
-                    coh = Vector2Add(coh, boids[j].pos);
+                    Vector2 diff = boids[i].pos - boids[j].pos;
+                    sep += (diff * (1.0f) / (d + 0.01f));
+                    ali += boids[j].vel;
+                    coh += boids[j].pos;
                     count++;
                 }
             }
 
             if (count > 0)
             {
-                ali = Vector2Scale(ali, 1.0f / count);
-                coh = Vector2Subtract(Vector2Scale(coh, 1.0f / count), boids[i].pos);
+                ali = (ali * 1.0f / count);
+                coh = (coh * 1.0f / count) - boids[i].pos;
             }
             float deltaTime = GetFrameTime();
-            boids[i].vel = Vector2Add(boids[i].vel, Vector2Add(Vector2Add(Vector2Scale(sep, deltaTime * SEP_W),
-                                                                          Vector2Scale(ali, deltaTime * ALI_W)),
-                                                               Vector2Scale(coh, deltaTime * COH_W)));
+            boids[i].vel += ali * ALI_W * deltaTime + coh * COH_W * deltaTime + sep * SEP_W * deltaTime;
 
-            boids[i].vel = limit(boids[i].vel, MAX_SPEED);
-            boids[i].pos = Vector2Add(boids[i].pos, boids[i].vel);
+            boids[i].vel = limit(boids[i].vel);
+            boids[i].pos = boids[i].pos + boids[i].vel;
 
-            if (boids[i].pos.x < 0)
-                boids[i].pos.x += WORLD_WIDTH;
-            if (boids[i].pos.y < 0)
-                boids[i].pos.y += WORLD_HEIGHT;
-            if (boids[i].pos.x > WORLD_WIDTH)
-                boids[i].pos.x -= WORLD_WIDTH;
-            if (boids[i].pos.y > WORLD_HEIGHT)
-                boids[i].pos.y -= WORLD_HEIGHT;
+            ClampToWorld(&boids[i]);
 
             Triangle boid_triangle = UpdateTriangleBoid(boids[i]);
             DrawTriangle(boid_triangle.v1, boid_triangle.v3, boid_triangle.v2, RAYWHITE);
         }
-        DrawRectangleLines(0, 0, WORLD_HEIGHT, WORLD_WIDTH, RED);
+        DrawRectangleLines(0, 0, WORLD_HEIGHT, WORLD_WIDTH, GREEN);
         EndMode2D();
         DrawFPS(WIDTH - 80, 0);
         EndDrawing();
